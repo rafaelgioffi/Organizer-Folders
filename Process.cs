58b5e63ac9011708e292102be223596920c2ca56
Rafael Gioffi <rafaelgioffi@gmail.com>
2024-03-21 11:15:40 -0300

    Final version

--- Process.cs
+++ Process.cs
@@ -68,8 +68,7 @@ namespace SortFIlesDown
                 }
             }
 
-            config = new ConfigurationBuilder()
-            //.SetBasePath(dirSettings)
+            config = new ConfigurationBuilder()            
             .AddJsonFile(fileSettings, optional: false, reloadOnChange: true)
             .Build();
 
@@ -99,26 +98,7 @@ namespace SortFIlesDown
         }
 
         private async Task GetFoldersAndFiles()
-        {
-            //string dirSettings = Path.GetFullPath($"{SpecialDirectories.MyDocuments}\\OrganizeFolders\\");
-            //string fileSettings = dirSettings + "appSettings.json";
-
-            //if (!File.Exists(fileSettings))
-            //{
-            //    Directory.CreateDirectory(Path.GetFullPath($"{SpecialDirectories.MyDocuments}\\OrganizeFolders"));
-            //    File.Copy(AppDomain.CurrentDomain.BaseDirectory + "..\\..\\..\\appSettings.json", fileSettings, true);
-            //}
-
-            //var config = new ConfigurationBuilder()
-            // .AddJsonFile(fileSettings, optional: false, reloadOnChange: true)
-            // .Build();
-
-            //IConfigurationRoot config = new ConfigurationBuilder()
-            ////.SetBasePath(dirSettings)
-            //.AddJsonFile(fileSettings, optional: false, reloadOnChange: true)
-            //.Build();
-
-
+        {   
             string folder1 = config["Folders:Folder1"];
             string folder2 = config["Folders:Folder2"];
             string folder3 = config["Folders:Folder3"];
@@ -131,8 +111,16 @@ namespace SortFIlesDown
             string type4 = config["FolderOrderType:Type4"];
             string type5 = config["FolderOrderType:Type5"];
 
+            string order1 = config["DateOrder:Order1"];
+            string order2 = config["DateOrder:Order2"];
+            string order3 = config["DateOrder:Order3"];
+            string order4 = config["DateOrder:Order4"];
+            string order5 = config["DateOrder:Order5"];
+
             List<string> folders = new();
             List<string> types = new();
+            List<string> orders = new();
+
             if (!string.IsNullOrEmpty(folder1))
                 folders.Add(folder1);
             if (!string.IsNullOrEmpty(folder2))
@@ -155,32 +143,34 @@ namespace SortFIlesDown
             if (!string.IsNullOrEmpty(type5))
                 types.Add(type5);
 
-            if (folders.Count != types.Count)
+            if (!string.IsNullOrEmpty(order1))
+                orders.Add(order1.ToUpper());
+            if (!string.IsNullOrEmpty(order2))
+                orders.Add(order2.ToUpper());
+            if (!string.IsNullOrEmpty(order3))
+                orders.Add(order3.ToUpper());
+            if (!string.IsNullOrEmpty(order4))
+                orders.Add(order4.ToUpper());
+            if (!string.IsNullOrEmpty(order5))
+                orders.Add(order5.ToUpper());
+
+            if (folders.Count != types.Count || types.Count != orders.Count)
             {
-                Log($"Configurações inválidas! Pastas: {folders.Count}. Tipos de pastas: {types.Count}.\nÉ necessário ter as mesmas configurações.");
+                Log($"Configurações inválidas! Pastas: {folders.Count}. Tipos de pastas: {types.Count} Ordenação por data: {orders.Count}.\nÉ necessário ter as mesmas configurações.");
                 return;
             }
 
             try
-            {
-                //var currentUser = @"C:\Users\SeuUser\";
-                //var fullPath = Path.Combine(currentUser, "Downloads");
-                //var queueFiles = Directory.EnumerateFiles(fullPath);
-
-                //foreach (string currentPath in folders)
+            {                
                 for (int i = 0; i < folders.Count; i++)
-                {
-                    //var queueFiles = Directory.EnumerateFiles(currentPath);
+                {                    
                     var queueFiles = Directory.EnumerateFiles(folders[i]);
                     if (queueFiles.Any())
                     {
                         Log($"Processando a pasta {folders[i]}");
-                        //return;
-
-                        //var fileInfo = new FileInfo(currentPath);
+                        
                         var fileInfo = new FileInfo(folders[i]);
-                        //var directoriesToMove = new List<string>();
-
+                        
                         foreach (var queueFile in queueFiles)
                         {
                             fileInfo = new FileInfo(queueFile);
@@ -188,11 +178,21 @@ namespace SortFIlesDown
                             switch (types[i])
                             {
                                 case "date":
-                                    string fileYear = fileInfo.CreationTime.Year.ToString();
-                                    string fileMonth = fileInfo.CreationTime.Month.ToString("D2");
-                                    //var folderYear = Path.Combine(currentPath, fileYear);
-                                    var folderYear = Path.Combine(folders[i], fileYear);
-                                    //var folderMonth = Path.Combine(currentPath, fileYear + "\\" + fileMonth);
+                                    string fileYear = "";
+                                    string fileMonth = "";
+
+                                    if (orders[i] == "M")
+                                    {
+                                        fileYear = fileInfo.LastWriteTime.Year.ToString();
+                                        fileMonth = fileInfo.LastWriteTime.Month.ToString("D2");
+                                    }
+                                    else if (orders[i] == "C")
+                                    {
+                                        fileYear = fileInfo.CreationTime.Year.ToString();
+                                        fileYear = fileInfo.CreationTime.Month.ToString("D2");
+                                    }
+                                                                       
+                                    var folderYear = Path.Combine(folders[i], fileYear);                                    
                                     var folderMonth = Path.Combine(folders[i], fileYear + "\\" + fileMonth);
 
                                     if (!Directory.Exists(folderYear))
@@ -207,9 +207,6 @@ namespace SortFIlesDown
                                         Log($"Criada a pasta {folderMonth}");
                                     }
 
-                                    //if (!directoriesToMove.Contains(folderMonth))
-                                    //directoriesToMove.Add(folderMonth);
-
                                     int fileNumber = 1;
                                     var result = fileExistsInPath(folderMonth, fileInfo.Name, fileInfo.Extension, fileNumber);
 
@@ -223,8 +220,7 @@ namespace SortFIlesDown
                                     }
                                     break;
 
-                                case "type":
-                                    //var folderName = Path.Combine(currentPath, fileInfo.Extension.Trim('.').ToLower());
+                                case "type":                                   
                                     var folderName = "";
                                     if (string.IsNullOrEmpty(fileInfo.Extension))
                                     {
@@ -241,9 +237,6 @@ namespace SortFIlesDown
                                         Log($"Criada a pasta {folderName}");
                                     }
 
-                                    //if (!directoriesToMove.Contains(folderName))
-                                    //    directoriesToMove.Add(folderName);
-
                                     fileNumber = 1;
                                     result = fileExistsInPath(folderName, fileInfo.Name, fileInfo.Extension, fileNumber);
 
@@ -259,30 +252,10 @@ namespace SortFIlesDown
                             }
                         }
                     }
-
-                    //var pathFiles = new List<string>();
-
-                    //foreach (var queueDirectories in directoriesToMove)
-                    //{
-                    //    foreach (var queue in queueFiles)
-                    //    {
-                    //        fileInfo = new FileInfo(queue);
-                    //        //var folderName = Path.Combine(queueDirectories, $"{fileInfo.LastWriteTimeUtc.ToShortDateString().Replace("/", "-")}");
-                    //        //var folderName = Path.Combine(queueDirectories, $"{fileInfo.CreationTime.Year}\\{fileInfo.CreationTime.Month}");
-
-                    //        //if (!Directory.Exists(folderName) && folderName.Contains(fileInfo.Extension.Replace(".", "-").ToLower()))
-                    //        //Directory.CreateDirectory(folderName).Create();
-
-                    //        //if (!pathFiles.Contains(folderName))
-                    //        if (!pathFiles.Contains(queueDirectories))
-                    //            //pathFiles.Add(folderName);
-                    //            pathFiles.Add(queueDirectories);
-                    //    }
-                    //}
-
-                    //MoveFiles(ref pathFiles, ref queueFiles, ref fileInfo);
-
-                    //pathFiles.Clear();
+                    else
+                    {
+                        Log($"Nenhum arquivo para processar na pasta {folders[i]}");
+                    }
                 }
             }
             catch (Exception ex)
@@ -300,14 +273,7 @@ namespace SortFIlesDown
                     fileInfo = new FileInfo(file);
 
                     foreach (var folder in pathFiles)
-                    {
-                        //var fileInfoDate = fileInfo.LastWriteTimeUtc.ToShortDateString().Replace("/", "-");
-
-
-                        //if (folder.EndsWith(fileInfoDate) && folder.ToLower().Contains(fileInfo.Extension.Replace('.', '-').ToLower()))
-                        //{
-
-                        //var result = fileExistsInPath(folder, fileInfo.Name, fileInfo.Extension);
+                    {                        
                         int fileNumber = 1;
                         var result = fileExistsInPath(folder, fileInfo.Name, fileInfo.Extension, fileNumber);
 
@@ -320,8 +286,7 @@ namespace SortFIlesDown
                             Log($"Falha ao mover o arquivo {file} para {result}. {ex.Message}");
                         }
 
-                        break;
-                        //}
+                        break;                       
                     }
                 }
             }
@@ -339,7 +304,6 @@ namespace SortFIlesDown
 
             file = file.Replace(extension, "");
 
-            //var newFileName = Path.Combine(directory, $"{file}-Copy-{DateTime.Now.Nanosecond}{extension}");
             var newFileName = Path.Combine(directory, $"{file} ({number}){extension}");
 
             while (File.Exists(newFileName))
@@ -353,15 +317,15 @@ namespace SortFIlesDown
         }
 
         public void ClearTemp()
-        {
-            //cleanTemp = bool.Parse(config["Settings:CleanTempData"]);
+        {         
             if (cleanTemp)
             {
                 string tempFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Temp\\");
                 string winTemp = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Windows), "Temp\\");
                 string prefetchDir = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Windows), "Prefetch\\");
-                try {
-                    Directory.Delete(tempFolder, true);                    
+                try
+                {
+                    Directory.Delete(tempFolder, true);
                 }
                 catch (Exception ex)
                 {
@@ -389,8 +353,7 @@ namespace SortFIlesDown
         }
 
         public void Log(string message, bool special = false)
-        {
-            //logging = bool.Parse(config["Settings:Log"]);
+        {         
             if (logging)
             {
                 using (StreamWriter sw = new StreamWriter(config["Logging:File"], true))
